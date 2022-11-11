1712acd1eb6d5886ba34700541250d3d04fb74a8
ayshajamjam <aj2604@columbia.edu>
2022-11-10 23:08:45 -0500

    Fixed errors to do wit NoneType; just restart cookies

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 """
 To run locally:
-    python3 server.py
+    virtual machine: source virtual/bin/activate
+    python3 app.py --debug
 Go to http://localhost:8111 in your browser.
 
 References (code taken from online sites):
 Login/Authentication: https://github.com/PrettyPrinted/youtube_video_code/blob/master/2020/02/10/Creating%20a%20Login%20Page%20in%20Flask%20Using%20Sessions/flask_session_example/app.py
 
+After cloning, make virtual env
+source virtual/bin/activate
+export FLASK_ENV=development
+export FLASK_APP=app.py
+Pip install flask
+pip install Flask-SQLAlchemy
+pip3 install Flask-WTF
+To fix psycopg2 error:
+sudo pip3 install psycopg2-binary
+pip install psycopg2-binary --user
+
+Tips: If you get a NoType error, just delete your cookies
 """
 
 import os
@@ -16,7 +29,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, IntegerField, DateField, TextAreaField
 from wtforms.validators import DataRequired, NumberRange
-# from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 
 # Create a Flask Instance
 app = Flask(__name__)
@@ -46,25 +58,37 @@ class NewReviewForm(FlaskForm):
 
 all_users = []
 
+@app.before_first_request
+def before_first_request():
+    try:
+        g.conn = engine.connect()
+
+        # New add ons
+        cursor = g.conn.execute("SELECT * FROM studentPatients")
+        for result in cursor:
+            all_users.append(result)
+        cursor.close()
+        
+    except:
+        print("uh oh, problem connecting to database")
+        import traceback; traceback.print_exc()
+        g.conn = None
+
 # Runs at beginning of every web request; sets up db connection
 @app.before_request
 def before_request():
-  try:
-    g.conn = engine.connect()
-    cursor = g.conn.execute("SELECT * FROM studentPatients")
-    for result in cursor:
-        all_users.append(result)
-    cursor.close()
+    try:
+        g.conn = engine.connect()
 
-    g.user = None
-    if session['user_uni']:
-        user = [x for x in all_users if x.uni == session['user_uni']][0]
-        g.user = user
+        g.user = None
+        if 'user_uni' in session:
+            user = [x for x in all_users if x.uni == session['user_uni']][0]
+            g.user = user
 
-  except:
-    print("uh oh, problem connecting to database")
-    import traceback; traceback.print_exc()
-    g.conn = None
+    except:
+        print("uh oh, problem connecting to database")
+        import traceback; traceback.print_exc()
+        g.conn = None
 
 # Runs at end of every web request; closes the database connection
 # If you don't, the database could run out of memory
@@ -84,6 +108,153 @@ def index():
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+@app.route('/doctors')
+def doctors():
+    cursor = g.conn.execute("SELECT * FROM doctors")
+    all_doctors = []
+    for result in cursor:
+        all_doctors.append(result)
+    cursor.close()
+    context = dict(data = all_doctors)
+
+    return render_template("doctors.html", **context)
+
+@app.route('/doctors/<npi>')
+def doctor(npi):
+    cursor = g.conn.execute("SELECT * FROM doctors")
+    doctor_info = []
+    for result in cursor:
+        if result['npi'] == npi:
+            doctor_info.append(result)
+    cursor.close()
+
+    cursor = g.conn.execute("SELECT * FROM doctors AS D, writes AS W, reviews AS R WHERE D.npi = W.npi AND W.review_id=R.review_id")
+    doctor_reviews = []
+    for result in cursor:
+        if result['npi'] == npi:
+            doctor_reviews.append(result)
+    cursor.close()
+    
+    context = dict(info = doctor_info[0], reviews = doctor_reviews)
+    return render_template("doctor.html", **context)
+
+@app.route('/users')
+def users():
+    cursor = g.conn.execute("SELECT * FROM studentPatients")
+    all_users = []
+    for result in cursor:
+        all_users.append(result)
+    cursor.close()
+    context = dict(data = all_users)
+    return render_template("users.html", **context)
+
+@app.route('/users/<uni>')
+def user(uni):
+
+    if not g.user or g.user.uni != uni:
+        flash("You do not have access to other students' profiles")
+        return redirect(url_for('users'))
+
+    # Get only user information
+    cursor = g.conn.execute("SELECT * FROM studentPatients")
+    user_info = []
+    for result in cursor:
+        if result['uni'] == uni:
+            user_info.append(result)
+    cursor.close()
+    
+    # Get specific user + saved doctors
+    cursor = g.conn.execute("SELECT * FROM studentPatients AS S, saves AS V, doctors AS D WHERE S.uni = V.uni AND D.npi=V.npi")
+    saved_doctors = []
+    for result in cursor:
+        if result['uni'] == uni:
+            saved_doctors.append(result)
+    cursor.close()
+
+    # Get specific user + reviews
+    cursor = g.conn.execute("SELECT * FROM studentPatients AS S, writes AS W, reviews AS R WHERE S.uni = W.uni AND W.review_id=R.review_id")
+    user_revs = []
+    for result in cursor:
+        if result['uni'] == uni:
+            user_revs.append(result)
+    cursor.close()
+    
+    # Get specific user + appointments
+    cursor = g.conn.execute("SELECT * FROM studentPatients AS S, doctors AS D, appointments AS A, schedules AS H WHERE S.uni = H.uni AND D.npi = H.npi AND A.apt_id=H.apt_id")
+    user_apts = []
+    for result in cursor:
+        if result['uni'] == uni:
+            user_apts.append(result)
+    cursor.close()
+
+    context = dict(info = user_info[0], saved = saved_doctors, reviews = user_revs, appointments = user_apts)
+    return render_template("user.html", **context)
+
+@app.route('/newUser', methods=['GET','POST'])
+def newUser():
+    first_name = None
+    last_name = None
+    uni = None
+    password = None
+    age = None
+    school = None
+    form = NewUserForm()
+    if form.validate_on_submit():
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        uni = form.uni.data
+        password = form.password.data
+        age = form.age.data
+        school = form.school.data
+        form.first_name.data = ''
+        form.last_name.data = ''
+        form.uni.data = ''
+        form.password.data = ''
+        form.age.data = ''
+        form.school.data = ''
+        flash("Form Submitted Successfully")
+    return render_template("newUser.html", 
+        first_name=first_name,
+        last_name = last_name,
+        uni = uni,
+        password = password,
+        age = age,
+        school = school,
+        form=form)
+
+@app.route('/reviews/<review_id>')
+def review(review_id):
+    cursor = g.conn.execute("SELECT * FROM doctors AS D, writes AS W, reviews AS R WHERE D.npi = W.npi AND W.review_id=R.review_id")
+    review_result = []
+    for result in cursor:
+        if result['review_id'] == int(review_id):
+            review_result.append(result)
+    cursor.close()
+    context = dict(data = review_result[0])
+
+    return render_template("review.html", **context)
+
+@app.route('/newReview/<npi>', methods=['POST', 'GET'])
+def newReview(npi):
+    date_of_visit = None
+    rating = None
+    review_content = None
+
+    form = NewReviewForm()
+    if form.validate_on_submit():
+        date_of_visit = form.date_of_visit.data
+        rating = form.rating.data
+        review_content = form.review_content.data
+        form.date_of_visit.data = ''
+        form.rating.data = ''
+        form.review_content.data = ''
+        flash("Form Submitted Successfully")
+    return render_template("newReview.html",
+        date_of_visit = date_of_visit,
+        rating = rating,
+        review_content = review_content,
+        form=form)
 
 @app.route('/login', methods=['GET','POST'])
 def login():
@@ -124,172 +295,6 @@ def logout():
     session['user_uni'] = None
     return redirect(url_for('index'))
 
-@app.route('/dashboard', methods=['GET','POST'])
-def dashboard():
-    return render_template("dashboard.html")
-
-@app.route('/newUser', methods=['GET','POST'])
-def newUser():
-    first_name = None
-    last_name = None
-    uni = None
-    password = None
-    age = None
-    school = None
-    form = NewUserForm()
-    if form.validate_on_submit():
-        first_name = form.first_name.data
-        last_name = form.last_name.data
-        uni = form.uni.data
-        password = form.password.data
-        age = form.age.data
-        school = form.school.data
-        form.first_name.data = ''
-        form.last_name.data = ''
-        form.uni.data = ''
-        form.password.data = ''
-        form.age.data = ''
-        form.school.data = ''
-        flash("Form Submitted Successfully")
-    return render_template("newUser.html", 
-        first_name=first_name,
-        last_name = last_name,
-        uni = uni,
-        password = password,
-        age = age,
-        school = school,
-        form=form)
-
-@app.route('/doctors')
-def doctors():
-    cursor = g.conn.execute("SELECT * FROM doctors")
-    all_doctors = []
-    for result in cursor:
-        all_doctors.append(result)
-    cursor.close()
-    context = dict(data = all_doctors)
-
-    return render_template("doctors.html", **context)
-
-@app.route('/doctors/<npi>')
-def doctor(npi):
-    cursor = g.conn.execute("SELECT * FROM doctors")
-    doctor_info = []
-    for result in cursor:
-        if result['npi'] == npi:
-            doctor_info.append(result)
-    cursor.close()
-
-    cursor = g.conn.execute("SELECT * FROM doctors AS D, writes AS W, reviews AS R WHERE D.npi = W.npi AND W.review_id=R.review_id")
-    doctor_reviews = []
-    for result in cursor:
-        if result['npi'] == npi:
-            doctor_reviews.append(result)
-    cursor.close()
-    
-    context = dict(info = doctor_info[0], reviews = doctor_reviews)
-    return render_template("doctor.html", **context)
-
-@app.route('/users')
-def users():
-    cursor = g.conn.execute("SELECT * FROM studentPatients")
-    all_users = []
-    for result in cursor:
-        all_users.append(result)
-    cursor.close()
-    context = dict(data = all_users)
-
-    return render_template("users.html", **context)
-
-@app.route('/users/<uni>')
-def user(uni):
-    if not g.user or g.user.uni != uni:
-        flash("You do not have access to other students' profiles")
-        return redirect(url_for('users'))
-
-    # Get only user information
-    cursor = g.conn.execute("SELECT * FROM studentPatients")
-    user_info = []
-    for result in cursor:
-        if result['uni'] == uni:
-            user_info.append(result)
-    cursor.close()
-    
-    # Get specific user + saved doctors
-    cursor = g.conn.execute("SELECT * FROM studentPatients AS S, saves AS V, doctors AS D WHERE S.uni = V.uni AND D.npi=V.npi")
-    saved_doctors = []
-    for result in cursor:
-        if result['uni'] == uni:
-            saved_doctors.append(result)
-    cursor.close()
-
-    # Get specific user + reviews
-    cursor = g.conn.execute("SELECT * FROM studentPatients AS S, writes AS W, reviews AS R WHERE S.uni = W.uni AND W.review_id=R.review_id")
-    user_revs = []
-    for result in cursor:
-        if result['uni'] == uni:
-            user_revs.append(result)
-    cursor.close()
-    
-    # Get specific user + appointments
-    cursor = g.conn.execute("SELECT * FROM studentPatients AS S, doctors AS D, appointments AS A, schedules AS H WHERE S.uni = H.uni AND D.npi = H.npi AND A.apt_id=H.apt_id")
-    user_apts = []
-    for result in cursor:
-        if result['uni'] == uni:
-            user_apts.append(result)
-    cursor.close()
-
-    context = dict(info = user_info[0], saved = saved_doctors, reviews = user_revs, appointments = user_apts)
-    return render_template("user.html", **context)
-
-@app.route('/reviews')
-def reviews():
-    cursor = g.conn.execute("SELECT * FROM reviews")
-    review_result = []
-    for result in cursor:
-        review_result.append(result)
-    cursor.close()
-    context = dict(data = review_result)
-
-    return render_template("reviews.html", **context)
-
-@app.route('/reviews/<review_id>')
-def review(review_id):
-    cursor = g.conn.execute("SELECT * FROM doctors AS D, writes AS W, reviews AS R WHERE D.npi = W.npi AND W.review_id=R.review_id")
-    review_result = []
-    for result in cursor:
-        if result['review_id'] == int(review_id):
-            review_result.append(result)
-    cursor.close()
-    context = dict(data = review_result[0])
-
-    return render_template("review.html", **context)
-
-@app.route('/newReview/<npi>', methods=['POST', 'GET'])
-def newReview(npi):
-    date_of_visit = None
-    rating = None
-    review_content = None
-
-    form = NewReviewForm()
-    if form.validate_on_submit():
-        date_of_visit = form.date_of_visit.data
-        rating = form.rating.data
-        review_content = form.review_content.data
-        form.date_of_visit.data = ''
-        form.rating.data = ''
-        form.review_content.data = ''
-        flash("Form Submitted Successfully")
-    return render_template("newReview.html",
-        date_of_visit = date_of_visit,
-        rating = rating,
-        review_content = review_content,
-        form=form)
-
-@app.route('/form')
-def form():
-    return render_template("form.html")
-
 if __name__ == "__main__":
   import click
 
@@ -298,10 +303,10 @@ if __name__ == "__main__":
   @click.option('--threaded', is_flag=True)
   @click.argument('HOST', default='0.0.0.0')
   @click.argument('PORT', default=8111, type=int)
-  
-  #This function handles command line parameters.
   def run(debug, threaded, host, port):
+
     HOST, PORT = host, port
     print("running on %s:%d" % (HOST, PORT))
     app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+
   run()

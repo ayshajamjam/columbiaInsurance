d644f17a5323b6a39f8a59c932a2e6803fb5eda9
ayshajamjam <aj2604@columbia.edu>
2022-12-06 15:19:29 +0300

    Fixed issue with array values

--- dataProjNew.sql
+++ dataProjNew.sql
@@ -3,7 +3,7 @@
 INSERT INTO Doctors 
 VALUES ('Timsi', 'Muttreja', 'Optometrist', 'OD','Dr. Timsi Muttreja is a compassionate optometrist driven to provide exceptional care to her patients in New York. Dr. Muttreja earned her Bachelor''s degree in Chemistry from SUNY Oswego. She received her Doctorate of Optometry from IAUPR School of Optometry, where she received the Award for Clinical Excellence. Dr. Muttreja provides comprehensive eye examinations and manages various ocular diseases including, but not limited to, dry eyes, cataracts, glaucoma and corneal related problems. She is fluent in Hindi, Punjabi, and Spanish. In her free time, Dr. Muttreja enjoys traveling, art and spending time with her family',
  '1669143079', 'https://d1k13df5m14swc.cloudfront.net/photos/Dr-Timsi-Muttreja-OD-353436-circle_large__v1__.png', 4.5, 'Universidad Interamericana de Puerto Rico', 32, '2128475075',
- {'dry eyes', 'cataracts', 'glaucoma', 'Annual Eye Exam', 'Contact Lenses', 'Eye Infection', 'General Eye Consultation'});
+ ARRAY ['dry eyes', 'cataracts', 'glaucoma', 'Annual Eye Exam', 'Contact Lenses', 'Eye Infection', 'General Eye Consultation']);
 
 INSERT INTO Hospitals
 VALUES ('Eyes on Park', '287 Park Ave South, New York, NY 10010', '110112', '3323452363');
@@ -24,7 +24,7 @@ VALUES ('Ellen', 'Edgar', 'Neurologist', 'MD',
 'Located in the heart of Forrest Hills, Dr. Ellen Edgar is a leading board certified neurologist and board certified neurophysiologist. Dr. Edgar specializes in treatment of muscular skeletal pain, eplilepsy and other types of neurological conditions including all types of headaches such as Botox injections for treatment of refractory migranes, cyrvical dystonya and other types of movement disorder.',
 '1326343625', 'https://d1k13df5m14swc.cloudfront.net/12493154-7705-4933-aca0-3901e9ac3e6bzoom.jpg',
 4.3, 'Andizhan State Medical Institute', 23, '8002008196',
-{'Back Pain', 'Headache', 'Migraine', 'Neurology Consultation', 'Sleep Problems', 'Neuromuscular Disorders', 'Spasticity Management'});
+ARRAY ['Back Pain', 'Headache', 'Migraine', 'Neurology Consultation', 'Sleep Problems', 'Neuromuscular Disorders', 'Spasticity Management']);
 
 INSERT INTO Hospitals
 VALUES ('Complete Neurological Care', '11247 Queens Blvd, Forest Hills, NY 11375', '120023', '8002008196');
@@ -45,7 +45,7 @@ VALUES ('Howard', 'Shaw', 'OB-GYN', 'MD',
 'Dr. Howard Shaw has been a practicing Obstetrician and Gynecologist since 1995. He graduated Cum Laude from Rutgers in 1987 where he earned his bachelor''s degree in biological science.',
 '1255379509', 'https://d2uur722ua7fvv.cloudfront.net/0e991dfc-cbe6-4e49-a923-6ecd82bb2ff9zoom.jpg',
 4.71, 'State University of New York', 13, '2125179048',
-{'Pap smear irregularities', 'annual check ups', 'pre and post- menopausal symptoms', 'obstetrics', 'gynecology'});
+ARRAY ['Pap smear irregularities', 'annual check ups', 'pre and post- menopausal symptoms', 'obstetrics', 'gynecology']);
 
 INSERT INTO Hospitals
 VALUES ('ROSH Maternal', '115 East 67th St, New York, NY 10065', '552304', '2122493949');
@@ -66,7 +66,7 @@ VALUES ('Jed', 'Kaminetsky', 'Urologist', 'MD',
 'Dr. Jed Kaminetsky, M.D. is a Clinical Associate Professor at New York University Medical Center, and is a practicing urologist with University Urology Associates, one of the largest urologic practices in the New York City metropolitan area. He is a board certified urologist and is highly skilled in treating all urologic disorders, performing minimally invasive prostate surgeries and penile implants. Dr. Kaminetsky’s academic interests include the study of both male and female sexual dysfunction, which has led him to become a national and world thought leader.',
 '1952378598', 'https://d2uur722ua7fvv.cloudfront.net/photos/Dr-Jed-Kaminetsky-MD-48127-circle_large__v1__.png',
 4.75, 'New York University', 55, '2126869015',
-{'benign prostatic hyperplasia', 'erectile dysfunction', 'low testosterone', 'vasectomy', 'Incontinence', 'Sexually Transmitted Disease (STD)', 'Urinary Tract Infection (UTI)'});
+ARRAY ['benign prostatic hyperplasia', 'erectile dysfunction', 'low testosterone', 'vasectomy', 'Incontinence', 'Sexually Transmitted Disease (STD)', 'Urinary Tract Infection (UTI)']);
 
 INSERT INTO Hospitals
 VALUES ('University Urology', '215 Lexington Avenue 21st Floor, New York, NY 10016', '445923', '2126869015');
@@ -87,7 +87,7 @@ VALUES ('Sameh', 'Elguizaoui', 'Orthopedic Surgeon', 'MD',
 'Sameh Elguizaoui, M.D. is a fellowship trained orthopedic surgeon who specializes in sports medicine and arthroscopic surgery as well as the treatment of general orthopedic injuries and trauma. In addition to his clinical interest in shoulder and knee disorders, Dr. Elguizaoui has a focused specialization on joint preservation and treatment of arthritis in young active patients with current regenerative medicine and cartilage repair and transplant techniques, as well as biologic alternatives to surgery.',
 '1568755460', 'https://d2uur722ua7fvv.cloudfront.net/photos/Dr-Sameh-Elguizaoui-MD-236423-circle_large__v2__.png',
 4.80, 'Ohio State University', 121, '2127373301',
-{'Cartilage Repair', 'Joint preservation', 'Arthroscopy of the shoulder, elbow, knee and ankle', 'ACL and ligament injuries of the knee', 'Cartilage repair and joint preservation', 'Cartilage transplantation', 'Meniscal tears', 'Meniscal transplantation', 'Shoulder instability and labral tears', 'Rotator cuff tears'});
+ARRAY ['Cartilage Repair', 'Joint preservation', 'Arthroscopy of the shoulder, elbow, knee and ankle', 'ACL and ligament injuries of the knee', 'Cartilage repair and joint preservation', 'Cartilage transplantation', 'Meniscal tears', 'Meniscal transplantation', 'Shoulder instability and labral tears', 'Rotator cuff tears']);
 
 INSERT INTO Hospitals
 VALUES ('NY Orthopedics', '159 E 74th St, New York, NY 10021', '330020', '2127373301');
@@ -108,7 +108,7 @@ VALUES ('Saleh', 'Rachidi', 'Dermatologist', 'MD',
 'Dr. Saleh Rachidi is a leading NYC dermatologist at Dermatology, Laser and Surgery of Flatiron, specializing in laser skin resurfacing and rejuvenation, body contouring, skin tightening, injectables, laser hair removal, vein treatments, hair loss treatments, and personalized skin care. Dr. Rachidi offers the latest cosmetic treatments and procedures. Patients trust Dr. Rachidi’s expertise because he consistently provides high-quality care that leads to positive and lasting health outcomes. Dr. Rachidi graduated from the Medical University of South Carolina (MUSC), where he received both his MD and PhD degrees. During his time at MUSC, he earned the Distinguished Graduate of the Year award and was inducted into the Alpha Omega Alpha Honor Medical Society. For his postgraduate training, Dr. Rachidi completed his residency in dermatology at Johns Hopkins University. He then finished his fellowship training in cosmetic dermatologic surgery. During his postgraduate training, Dr. Rachidi received awards from the Society of Investigative Dermatology, the American Society for Dermatologic Surgery, and the American Society for Laser Medicine and Surgery, and also received a research grant from the Center for Clinical Data Analysis. He is pleased to bring his dermatologic expertise to Dermatology, Laser and Surgery of Flatiron.',
 '1447609615', 'https://d1k13df5m14swc.cloudfront.net/457ac590-bf0a-4695-8277-e5c48699c5d1zoom.jpg',
 4.61, 'Medical University of South Carolina', 3, '8443376362',
-{'Acne', 'Allergy Consultation', 'Annual Skin Screening', 'Dermatology Consultation', 'Sexually Transmitted Disease (STD)', 'laser skin resurfacing and rejuvenation', 'body contouring', 'skin tightening', 'injectables', 'laser hair removal', 'vein treatments', 'hair loss treatments', 'personalized skin care'});
+ARRAY ['Acne', 'Allergy Consultation', 'Annual Skin Screening', 'Dermatology Consultation', 'Sexually Transmitted Disease (STD)', 'laser skin resurfacing and rejuvenation', 'body contouring', 'skin tightening', 'injectables', 'laser hair removal', 'vein treatments', 'hair loss treatments', 'personalized skin care']);
 
 INSERT INTO Hospitals
 VALUES ('Dermatology Laser & Surgery of Carnegie Hill PLLC', '1095 Park Ave New York, NY 10128', '310254', '6466616131');
@@ -129,7 +129,7 @@ VALUES ('Daniel', 'Demesh', 'Ear, Nose & Throat Doctor', 'MD',
 'Daniel Demesh, MD is a member of the Facial Plastic and Reconstructive Surgery and Ear, Nose, and Throat (ENT) teams. Dr. Demesh is a fellowship-trained facial plastic surgeon specializing in facial and neck rejuvenation.',
 '1629485586', 'https://d2uur722ua7fvv.cloudfront.net/f9bdb3a4-4b91-4a55-9ed8-9d1168975235zoom.jpg',
 4.62, 'Albert Einstein College of Medicine of Yeshiva University', 8, '6465937200',
-{'face-lifts', 'nose reshaping', 'eyelid surgery', 'chronic sinusitis', 'nasal congestion', 'deviated septum', 'nasal polyps', 'turbinate hypertrophy', 'chronic rhinitis'});
+ARRAY ['face-lifts', 'nose reshaping', 'eyelid surgery', 'chronic sinusitis', 'nasal congestion', 'deviated septum', 'nasal polyps', 'turbinate hypertrophy', 'chronic rhinitis']);
 
 INSERT INTO Hospitals
 VALUES ('Summit Health - New York', '1865 Broadway New York, NY 10023', '237789', '6465937200');
@@ -150,7 +150,7 @@ VALUES ('Jaskaran', 'Grewal', 'Psychiatrist', 'MD',
 'Expert in treating all psychiatric conditions. Employ a targeted approach utilizing the biopsychosocial model to target appropriate medications, and utilized the appropriate therapy for mental well being. Years of success in treating the most refractory of cases. We work together to form the strongest therapeutic alliance so you can be well. 10 years of experience managing all psychiatric conditions. Excellence and mastery of psychopharmacology and drug interactions. Successfully have treated the most difficult and refractory of all psychiatric pathologies. Will see patients on weekends. Let''s get you better together! We''ll work to treat you together and then maintain the success as we strive for new heights across the board!',
 '1053808535', 'https://d3wnzga3fpd9a.cloudfront.net/96ee66ad-1b17-462c-b97f-277bd65421b9zoom.jpg',
 4.93, 'American University of Antigua College of Medicine', 39, '6468498413',
-{'Anxiety', 'Depression', 'Hyperactive Disorder (ADD, ADHD)', 'Prescription and Refill', 'Psychiatry Consultation'});
+ARRAY ['Anxiety', 'Depression', 'Hyperactive Disorder (ADD, ADHD)', 'Prescription and Refill', 'Psychiatry Consultation']);
 
 INSERT INTO Hospitals
 VALUES ('NYC Psychiatric Associates', '353 Lexington Ave New York, NY 10016', '900021', '9173910076');
@@ -171,7 +171,7 @@ VALUES ('Anna', 'Kezerashvili', 'Cardiologist', 'MD',
 'Anna Kezerashvili, MD, FACC is a caring and dedicated cardiologist serving the community of Bronx and New York, NY. Dr. Kezerashvili attended the Albert Einstein College of Medicine of Yeshiva University, where she received her MD with HONORS and completed her residency in internal medicine at the same university. She completed a fellowship in cardiology at Winthrop-University Hospital. Dr. Kezerashvili is a part of KezMed Medical PC. She is board certified in internal medicine and cardiovascular disease by the American Board of Internal Medicine.',
 '1245528975', 'https://d1k13df5m14swc.cloudfront.net/92f5b50c-edac-41dc-9219-f53e7890b521zoom.jpg',
 4.71, 'Albert Einstein College of Medicine of Yeshiva University', 43, '6465308447',
-{'Vascular Interpretations (RPVI)', 'aortic aneurysm', 'venous insufficiency', 'varicose veins', 'peripheral arterial disease'});
+ARRAY ['Vascular Interpretations (RPVI)', 'aortic aneurysm', 'venous insufficiency', 'varicose veins', 'peripheral arterial disease']);
 
 INSERT INTO Hospitals
 VALUES ('KezMed Medical PC', '1963 Williamsbridge Rd Bronx, NY 10461', '323276', '6465308447');
@@ -192,7 +192,7 @@ VALUES ('Purvi', 'Parikh', 'Allergist', 'MD',
 'Dr. Purvi Parikh is an allergist with a specialization in pediatric allergies. She works with Allergy and Asthma Associates of Murray Hill at their Hauppauge, New York, Forest Hills, and Huntington, NY locations. Patients visit Dr. Parikh to seek treatment for concerns and conditions, such as hives, hay fever, asthma, drug allergies, and eczema. She strives to stay updated with technological advancements in medicine to incorporate them into her treatment plans. Dr. Parikh earned her medical degree from the St. George''s University School of Medicine. She has been certified by the American Board of Allergy and Immunology and the American Board of Internal Medicine.',
 '1629212592', 'https://d1k13df5m14swc.cloudfront.net/bba7ccdc-113d-46e7-94e3-0bea1f76fe18zoom.jpg',
 4.63, 'St. George''s University School of Medicine', 15, '7182612661',
-{'Infectious Disease Allergy', 'Immunology', 'Pediatric Allergy', 'allergy', 'asthma', 'common variable immunodeficiency', 'hives', 'hay fever'});
+ARRAY ['Infectious Disease Allergy', 'Immunology', 'Pediatric Allergy', 'allergy', 'asthma', 'common variable immunodeficiency', 'hives', 'hay fever']);
 
 INSERT INTO Hospitals
 VALUES ('Allergy and Asthma Associates of Murray Hill', '161 Madison Ave New York, NY 10016', '458099', '2126854225');
